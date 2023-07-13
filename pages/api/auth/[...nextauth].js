166c7b949997d081679b1694734a6ad66b9e7e48
Hassan Jarko <hassan.jarko@yahoo.com>
2023-07-14 01:05:24 +0400

    added next_secret and updated nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,6 +18,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     session: async ({session,token,user}) => {
